dc3e6f6decc284af4ddfa06d83b0e5fa8a485891
Oliver Zihler <github@codeartify.com>
2025-03-06 17:52:54 +0100

    exercise

--- src/test/java/com/codeartify/parking_spot/reservation/controller/ParkingSpotReservationControllerApprovalTestShould.java
+++ src/test/java/com/codeartify/parking_spot/reservation/controller/ParkingSpotReservationControllerApprovalTestShould.java
@@ -1,27 +1,11 @@
 package com.codeartify.parking_spot.reservation.controller;
 
-import com.codeartify.parking_spot.reservation.dto.ParkingReservationRequest;
-import com.codeartify.parking_spot.reservation.service.ParkingSpotReservationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
-
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParkingSpotReservationControllerApprovalTestShould {
     @Test
     void cover_reserve_parking_spot() {
-        var parkingSpotReservationService = new ParkingSpotReservationService(null, null);
-        var unitUnderTest = new ParkingSpotReservationController(parkingSpotReservationService);
-        String reservedBy = null;
-        var startTime = LocalDateTime.of(2025, 2, 23, 8, 0);
-        var endTime = LocalDateTime.of(2025, 2, 23, 8, 29);
-        ParkingReservationRequest request = new ParkingReservationRequest(reservedBy, startTime, endTime);
-
-        var result = unitUnderTest.reserveParkingSpot(request);
-
-        assertEquals("Body: Reservation must be at least 30 minutes long. status code: 400 BAD_REQUEST", stateAsString(result));
     }
 
     private String stateAsString(ResponseEntity<Object> result) {
