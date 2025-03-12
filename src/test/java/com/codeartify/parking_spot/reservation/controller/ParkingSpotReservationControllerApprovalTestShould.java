5a1fc1af5a797328b8360775a347d3719101b62f
Oliver Zihler <github@codeartify.com>
2025-03-12 12:38:04 +0100

    remove

--- src/test/java/com/codeartify/parking_spot/reservation/controller/ParkingSpotReservationControllerApprovalTestShould.java
+++ src/test/java/com/codeartify/parking_spot/reservation/controller/ParkingSpotReservationControllerApprovalTestShould.java
@@ -1,7 +1,6 @@
 package com.codeartify.parking_spot.reservation.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -9,10 +8,6 @@ class ParkingSpotReservationControllerApprovalTestShould {
     @Test
     void cover_reserve_parking_spot() {
         fail();
-    }
-
-    private String stateAsString(ResponseEntity<Object> result) {
-        return "Body: " + result.getBody() + " status code: " + result.getStatusCode();
     }
 
 }
